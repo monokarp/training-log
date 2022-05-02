86dd01eabdcf8b9c2ae411acb61c13948593478e
Oleh Bondarenko <oleh.bondarenko@dev.pro>
2022-05-02 10:44:57 +0300

    updated portal to display test entity and ran it

--- apps/portal/src/app/app.component.ts
+++ apps/portal/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Message } from '@training-log/api-interfaces';
+import { Test } from '@contracts';
 
 @Component({
-  selector: 'training-log-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+	selector: 'training-log-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
+	hello$ = this.http.get<Test>('/api/test');
+
+	constructor(private http: HttpClient) {}
 }
